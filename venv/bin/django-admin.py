332f73dd65f22757d502e18fecd2113a965dcb76
Jeffrenken <renkenjj@hotmail.com>
2018-01-28 17:12:31 -0600

    remove top folder

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Jeff/Desktop/django-haystack-elasticsearch-master/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
