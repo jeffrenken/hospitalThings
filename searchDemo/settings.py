c665f310e191ce5ed4def177fb3ac8eab64fbf3f
Jeff <renkenjj@hotmail.com>
2018-01-29 13:07:22 -0600

    additional files and changes for Heroku2

--- searchDemo/settings.py
+++ searchDemo/settings.py
@@ -117,8 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = ''
+#STATIC_ROOT = ''
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'searchDemo/static')
 STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
+
 MEDIA_ROOT = BASE_DIR
 MEDIA_URL = '/media/'
+
